17b90d2be8801b67a37922dc820b01bc75264701
Marek Brajerski <19328@student.ans-elblag.pl>
2023-08-04 20:10:33 +0200

    Update

---
+++ components/ImagesContainer.jsx
@@ -0,0 +1,40 @@
+
+    const containerItems = [
+        {
+            imgSrc: "./images/img1.png",
+            imgAlt: "img1",
+            title: "Using Abstract",
+            text: "Abstract lets you manage, version, and document your designs in one place.",
+          },
+          {
+            imgSrc: "https://theme.zdassets.com/theme_assets/2197739/91588b3d042df901d9b941ccbb57af4f151401c7.png",
+            imgAlt: "img2",
+            title: "Manage your account",
+            text: "Configure your account settings, such as your email, profile details, and password.",
+          },
+          {
+            imgSrc: "https://theme.zdassets.com/theme_assets/2197739/50d0db451c0e1e66e1caddd142986fcf6792ca05.png",
+            imgAlt: "img3",
+            title: "Manage organizations, teams, and projects",
+            text: "Use Abstract organizations, teams, and projects to organize your people and your work.",
+          },
+          {
+            imgSrc: "https://theme.zdassets.com/theme_assets/2197739/3e82d0126e97d7b7c60a611d4e11b3c278d33743.png",
+            imgAlt: "img4",
+            title: "Manage billing",
+            text: "Change subscriptions and payment details.",
+          },
+          {
+            imgSrc: "https://theme.zdassets.com/theme_assets/2197739/7712592465ef5c7bf18f1c7cb16e19ab44eb81e3.png",
+            imgAlt: "img5",
+            title: "Authenticate to Abstract",
+            text: "Set up and configure SSO, SCIM, and Just-in-Time provisioning.",
+          },
+          {
+            imgSrc: "https://theme.zdassets.com/theme_assets/2197739/dcc25d18e224d12621d74e545cc8bdd1b5685ca8.png",
+            imgAlt: "img6",
+            title: "Abstract support",
+            text: "Get in touch with a human.",
+          }
+    ];
+    export { containerItems };
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,15 +1,38 @@
+import { containerItems } from './ImagesContainer';
+
 export default function Main(){
-    return(
+    return (
         <main role="main" className="main">
-            <section className="hero-unit"> 
-                <div className="sectionContainer">
-                    <h1 className="sectionTag">How can we help?</h1>
-                    <input className="input1" placeholder="Search" type="text"></input>
-                </div>
-            </section>    
-            <section className="MiddlePage">
-                
-            </section> 
-        </main>    
-    )
-}
+            
+          <section className="hero-unit">
+            <div className="sectionContainer">
+              <h1 className="sectionTag">How can we help?</h1>
+              <input className="input1" placeholder="Search" type="text" />
+            </div>
+          </section>
+
+          <section className="MiddlePage">
+            <div className="Container">
+
+                {containerItems.map((item, index) => (
+
+                    <div className="ContainerItem" key={index}>
+
+                        <div className="ContainerIMG">
+                            <img className={`img1`} src={item.imgSrc} alt={item.imgAlt} />
+                        </div>
+
+                        <div className="ContainerContent">
+                            <h3>{item.title}</h3>
+                            <p className="ContainerContent_Text">{item.text}</p>
+                            <a>Learn More →</a>
+                        </div>
+
+                    </div>
+                ))}
+
+             </div>
+            </section>
+        </main>
+      );
+    }
